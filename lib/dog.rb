086fa7e5f5b32e3da6df059204dd2cc4da77462d
Veronica Kim <vero.a.kim@gmail.com>
2018-05-02 21:17:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,10 +28,11 @@ class Dog
   end
 
   def self.new_from_db(row)
+    new_dog = self.new
     new_dog.id = row[0]
     new_dog.name = row[1]
     new_dog.breed = row[2]
-    new_dog = 
+    new_dog
   end
 
 
