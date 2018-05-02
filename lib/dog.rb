abb4cc78cc7e812bed0c14095987ffc8b1f02572
Veronica Kim <vero.a.kim@gmail.com>
2018-05-02 21:52:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -57,7 +57,7 @@ class Dog
     self
   end
 
-  def self.find_by_id(id)
+  def self.find_by_id(row)
     sql =<<-SQL
       "SELECT * FROM dogs WHERE id = ?"
       result = DC[:conn].execute(sql, id)[0]
