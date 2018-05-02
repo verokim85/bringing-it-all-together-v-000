e7b3c04c304d5a29e3205729a8834446c77d7090
Veronica Kim <vero.a.kim@gmail.com>
2018-05-02 22:29:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -88,12 +88,12 @@ class Dog
       WHERE id = ?
       LIMIT 1
     SQL
-  
+
   DB[:conn].execute(sql, id).map do |row|
       self.new_from_db(row)
     end.first
   end
-  
+
 
   def update
     sql = <<-SQL
