4e34e503b783a6de6d782f23f9dd4cdcf4eacabf
Veronica Kim <vero.a.kim@gmail.com>
2018-05-02 22:33:20 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -87,7 +87,7 @@ class Dog
       dog_data = dog[0]
       dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
     else
-      dog = self.create(name: name, album: album)
+      dog = self.create(name: name, breed: breed)
       end
     dog
   end
